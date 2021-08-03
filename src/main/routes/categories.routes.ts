24ce197201d119461f172032204417a9852952bd
Mathues fellipi <matheus.fellipi@outlook.com>
2021-08-03 12:03:09 -0300

    chamando o serviço de criação na rota

--- src/main/routes/categories.routes.ts
+++ src/main/routes/categories.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { CategoryRepository } from "../../repositories/CategoriesRepository";
+import { CreateCategoryServices } from "../../services/CreateCategoryServices";
 
 const categoriesRoutes = Router();
 const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-
+  const createCategoryServices = new CreateCategoryServices(categoryRepository);
+  createCategoryServices.execute({ name, description });
   return response.status(201).send();
 });
 categoriesRoutes.get("/", (request, response) => {
--- src/services/CreateCategoryServices.ts
+++ src/services/CreateCategoryServices.ts
@@ -1,17 +1,19 @@
+import { CategoryRepository } from "../repositories/CategoriesRepository";
+
 interface IRequest {
   name: string;
   description: string;
 }
 // services nao precisa conhecer o request e os response
 class CreateCategoryServices {
-  constructor() {}
-  execute({ description, name }: IRequest) {
-    const categoryAlreadyExists = categoryRepository.findByName(name);
+  constructor(private categoriesRepository: CategoryRepository) {}
+  execute({ description, name }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
     if (categoryAlreadyExists) {
       throw new Error("Category already exists!");
     }
 
-    categoryRepository.create({ name, description });
+    this.categoriesRepository.create({ name, description });
   }
 }
 export { CreateCategoryServices };
