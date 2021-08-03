34d137ce7eef1719fbe11f49bafd94a9b581ae1c
Mathues fellipi <matheus.fellipi@outlook.com>
2021-08-03 17:47:44 -0300

    criando a função de stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,18 @@
+import fs from "fs";
+import cvsParse from "csv-parse";
+
 class ImportCategoryUseCase {
   constructor() {}
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = cvsParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 export { ImportCategoryUseCase };
