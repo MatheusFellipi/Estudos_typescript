8d6d8974d288b2571114e2b2944accbbc2723ab3
Mathues fellipi <matheus.fellipi@outlook.com>
2021-08-04 19:08:15 -0300

    criando o databe do postgres

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+import "../database/index";
 import { router } from "./routes";
 import swaggerFile from "../swagger.json";
 
