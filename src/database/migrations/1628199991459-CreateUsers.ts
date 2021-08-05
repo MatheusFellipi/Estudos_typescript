bfae7335116e44cc38c1af96b829d9d7a8627cd5
Mathues fellipi <matheus.fellipi@outlook.com>
2021-08-05 18:52:05 -0300

    Recriando a migration

---
+++ src/database/migrations/1628199991459-CreateUsers.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1628199991459 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "username",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "drive_license",
+            type: "varchar",
+          },
+          {
+            name: "isAdmin",
+            type: "boolean",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
