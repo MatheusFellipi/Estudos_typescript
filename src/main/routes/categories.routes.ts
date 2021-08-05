7cef29d911f20c36dac996335c4e855e7ac29490
Mathues fellipi <matheus.fellipi@outlook.com>
2021-08-05 12:16:05 -0300

    criando as migrations

--- src/main/routes/categories.routes.ts
+++ src/main/routes/categories.routes.ts
@@ -9,7 +9,6 @@ const categoriesRoutes = Router();
 const upload = multer({ dest: "./tmp" });
 
 categoriesRoutes.post("/", (request, response) => {
-  console.log("oi");
   return createCategoryController.handle(request, response);
 });
 categoriesRoutes.get("/", (request, response) => {
