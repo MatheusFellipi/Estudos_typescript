177cda7fd0dc713e1a9d2d210397af5dbc011f2a
Mathues fellipi <matheus.fellipi@outlook.com>
2021-08-05 18:53:00 -0300

    Criando o container de repositório de usuário

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,12 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoryRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
 import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
+
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoryRepository",
@@ -13,4 +17,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
   "SpecificationRepository",
   SpecificationRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 );
