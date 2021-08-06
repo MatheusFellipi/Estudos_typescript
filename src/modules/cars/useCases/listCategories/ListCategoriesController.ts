6155c10c30424bc060c0bfde26fea4b6a925fc49
Mathues fellipi <matheus.fellipi@outlook.com>
2021-08-05 23:35:51 -0300

    retornando o status 200 par listagem

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -8,7 +8,7 @@ class ListCategoriesController {
     const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
     const all = await listCategoriesUseCase.execute();
 
-    return response.status(201).json(all);
+    return response.status(200).json(all);
   }
 }
 
