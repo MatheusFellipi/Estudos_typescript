22553fcfdd1cc3bd3e9c524b235fe2df33ce3195
Mathues fellipi <matheus.fellipi@outlook.com>
2021-08-06 13:29:52 -0300

    criando a função de autenticação

---
+++ src/main/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { AppError } from "../../errors/appError";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+interface IPayLoad {
+  sub: string;
+}
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "5221ec70259819bba5acc33bbcdac8cf"
+    ) as IPayLoad;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exists!", 401);
+    }
+    request.user = {
+      id: user_id,
+    };
+    next();
+  } catch (error) {
+    throw new AppError("Invalid token", 401);
+  }
+}
